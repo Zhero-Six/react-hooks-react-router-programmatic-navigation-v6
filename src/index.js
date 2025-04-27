23addb61c2ecc4f6075e39eee0ada5474745f738
Zhero-Six <vmochoge@gmail.com>
2025-04-27 22:41:55 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import routes from "./routes"
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
 
-const router = createBrowserRouter(routes)
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router}/>)
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
